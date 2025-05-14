d7c5a1695bbdaec027565317d853f4dcd7bb3da4
Samuel Spink <72315013+x47base@users.noreply.github.com>
2025-05-14 14:33:48 +0200

    patch: merge error

--- freedb/commands/__init__.py
+++ freedb/commands/__init__.py
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 from .config import config_data_path
-from .create import create_table
-from .delete import drop_table, delete_query
-=======
 from .create import create_table, insert, create_database
-from .delete import delete
->>>>>>> 5313faf16d43237cef23d1f37ffe1a9d8bc6ac99
+from .delete import drop_table, delete_query
 from .read import read
 from .update import update
 from .use import use
