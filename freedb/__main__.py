450a88c224ae3e4f7e6fe0b548aa4bd00b5398e2
TrachselRobin <trachselr@bzz.ch>
2025-05-14 10:04:50 +0200

    ADDED: basic functions

--- freedb/__main__.py
+++ freedb/__main__.py
@@ -1,6 +1,2 @@
-from commands.create import *
-from commands.delete import *
-from commands.read import *
-from commands.update import *
-
 if __name__ == '__main__':
+    print("Welcome to FreeDB")
--- freedb/database.py
+++ freedb/database.py
@@ -1,15 +1,38 @@
+from commands.create import *
+from commands.delete import *
+from commands.read import *
+from commands.update import *
+
 class freeDB:
     def __init__(self):
         self._database_list = []
         self._active_database = None
 
     def createDatabase(self, database_name):
+        """
+        create a new database
+
+        :param database_name:
+        :return:
+        """
         pass
 
     def dropDatabase(self, database_name):
+        """
+        delete the database
+
+        :param database_name:
+        :return:
+        """
         pass
 
     def use(self, database_name):
+        """
+        To switch to the database
+
+        :param database_name:
+        :return:
+        """
         pass
 
 
@@ -17,8 +40,4 @@ class Database:
     def __init__(self, name):
         self._name = name
 
-    def createTable(self, table_name):
-        pass
-
-    def dropTable(self, table_name):
-        pass
+    # functions
